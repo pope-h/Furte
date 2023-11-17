12ff5ec0d1e92c2f0e455c2d87a7602856108395
Ekarika Nsemeke <hamatthan@gmail.com>
2023-11-17 05:38:21 -0800

    added cursor-pointer to the product page

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ const ProductCard = (props) => {
     return (
         <section className="w-full h-full group">
             <div className="overflow-hidden w-full h-[75%]">
-                <div className="relative w-[100%] h-[100%] transition-transform duration-300 ease-in-out transform hover:scale-105">
+                <div className="relative cursor-pointer w-[100%] h-[100%] transition-transform duration-300 ease-in-out transform hover:scale-105">
                     <img
                     src={props.src}
                     srcSet={`
@@ -25,7 +25,7 @@ const ProductCard = (props) => {
                     <div className="absolute inset-0 bg-black opacity-50"></div>
                 </div>
             </div>
-            <div className="inset-0 flex flex-col items-center justify-center text-black">
+            <div className="inset-0 flex flex-col items-center justify-center text-black cursor-pointer">
                 <h2 className="text-2xl leading-normal font-palanquin  group-hover:text-coral-red transition-colors">{props.title}</h2>
                 <p className="font-montserrat text-xl leading-normal text-slate-gray  group-hover:text-coral-red transition-colors">furte</p>
             </div>
