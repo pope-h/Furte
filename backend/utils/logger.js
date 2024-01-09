e524ef6cc35c743cc25d8ff7f935116b49e072f6
Ekarika Nsemeke <hamatthan@gmail.com>
2024-01-09 07:28:50 +0100

    adjusted the error from winston

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,8 +1,25 @@
-import winston from "winston";
+const winston = require("winston");
 
 const logger = winston.createLogger({
-  level: "info", // Adjust as needed
-  transports: [new winston.transports.File({ filename: "app.log" })],
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
 });
 
-export default logger;
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+module.exports = logger;
