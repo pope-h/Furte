ac549db6b76a1894de1302d8417a638fc6d5219a
Ekarika Nsemeke <hamatthan@gmail.com>
2024-01-11 01:23:37 +0100

    sorting error

--- frontend/src/pages/AdminProducts.jsx
+++ frontend/src/pages/AdminProducts.jsx
@@ -53,7 +53,7 @@ const AdminProducts = () => {
         console.log('Deleting product:', id, token);
         const response = await axios.delete(
           '/products',
-          { data: { id } },
+          { id },
           config
         );
         await handleApiError(response);
